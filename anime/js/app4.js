38b7eff42662fa45a5e8322762751aefc4512b95
Hassan El-Deghedy <106387607+LoLoch0@users.noreply.github.com>
2022-06-06 15:00:21 +0200

    Delete anime/js directory

--- anime/js/app4.js
+++
@@ -1,8 +0,0 @@
-function copyToClipboard(element) {
-    var $temp = jQuery("<input>");
-    jQuery("body").append($temp);
-    $temp.val(jQuery(element).val()).select();
-    document.execCommand("copy");
-    $temp.remove();
-    jQuery(".copied").show();
-}
